c8c0e24258d771fbe0ca45440fda62534afadc7a
ngadiono <ngadiono@djavaui.com>
2019-07-25 18:10:27 +0700

    adding addon storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,31 @@
 import { configure, addParameters } from '@storybook/react';
 import theme from './theme';
 
+const newViewports = {
+  kindleFire2: {
+    name: 'Kindle Fire 2',
+    styles: {
+      width: '600px',
+      height: '963px',
+    },
+  },
+  kindleFireHD: {
+    name: 'Kindle Fire HD',
+    styles: {
+      width: '533px',
+      height: '801px',
+    },
+  },
+};
+
 // Option defaults.
 addParameters({
   options: {
     theme: theme,
   },
+  viewport: { 
+    viewports: newViewports 
+  }
 });
 
 // automatically import all files ending in *.stories.js
--- .storybook/theme.js
+++ .storybook/theme.js
@@ -13,16 +13,12 @@ export default create({
   appBorderRadius: 4,
 
   // Typography
-  fontBase: '"Open Sans", sans-serif',
+  fontBase: '"Montserrat", sans-serif',
   fontCode: 'monospace',
 
   // Text colors
   textColor: 'black',
   textInverseColor: 'rgba(255,255,255,0.9)',
-
-  // Toolbar default and active colors
-  barTextColor: 'silver',
-  barSelectedColor: 'black',
 
   // Form colors
   inputBg: 'white',
