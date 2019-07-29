d8242b2fc416ff9d6a0057670e1a01c4f975af0e
ngadiono <ngadiono@djavaui.com>
2019-07-29 15:08:45 +0700

    adding style

---
+++ components/style/color/theme.js
@@ -0,0 +1,10 @@
+const themeRed = '#F04B32',
+      themeYellow = '#FFA000',
+      themeBlack = '#474955',
+      themeWhite = '#FFFFFF',
+      themeLightYellow = '#FFF5E5';
+      
+export { 
+    themeRed, themeYellow, themeBlack,
+    themeWhite, themeLightYellow
+};
