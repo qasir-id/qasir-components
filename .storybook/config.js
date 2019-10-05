794b7111417d805f3a4755c0830af0ee6623c754
adhitiadarmawan <adhit@qasir.id>
2019-10-06 00:22:53 +0700

    [UPDATE] Storybook config and library

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,20 @@
 import { configure, addParameters, addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
 
 import theme from './theme';
 
 // Addons components
 addDecorator(withA11y);
+addDecorator(withKnobs);
+
+addDecorator(
+  withInfo({
+    header: false,
+    // inline: true, 
+  })
+);
 
 // Option defaults.
 addParameters({
