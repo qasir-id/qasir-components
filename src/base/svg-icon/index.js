c453a502cbb5214605fcdd6f22bf342a8fe0a5f1
adhitiadarmawan <adhit@qasir.id>
2019-10-14 14:18:34 +0700

    delete for temporary

--- src/base/svg-icon/index.js
+++
@@ -1,3 +0,0 @@
-import SvgIcon from './svgicon';
-
-export default SvgIcon;
--- src/base/svg-icon/svg/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Menu } from './Menu';
-export { default as Report } from './Report';
-export { default as Notification } from './Notification';
