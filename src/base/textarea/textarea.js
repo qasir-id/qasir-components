458edb7f370005f502f69f3b0214bda067d617df
adhitiadarmawan <adhit@qasir.id>
2019-10-23 15:43:56 +0700

    [ADD] Textarea components

---
+++ src/base/textarea/textarea.js
@@ -0,0 +1,124 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import Text from '../text';
+
+import * as Style from './textarea.styles';
+
+class Textarea extends PureComponent {
+  constructor(props) {
+    super();
+    this.state = {
+      characterCount: (props.value && props.value.length) || 0,
+      currVal: props.value
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      currVal: e.target.value,
+      characterCount: e.target.value.length
+    });
+  };
+
+  render() {
+    const active = this.state.currVal !== '';
+
+    const {
+      className,
+      rows,
+      label,
+      value,
+      isDisabled,
+      error,
+      errorMessage,
+      maxLength,
+      showCharacterCount,
+      ...props
+    } = this.props;
+    return (
+      <>
+        <Style.Container {...props} data-error={error ? 'true' : undefined}>
+          <Style.Field
+            className={className}
+            disabled={isDisabled}
+            onChange={this.handleInputChange}
+            value={this.state.currVal}
+            rows={rows}
+            maxLength={maxLength}
+          />
+          <Style.Label data-active={active}>{label}</Style.Label>
+        </Style.Container>
+        {showCharacterCount && (
+          <Text
+            tag="div"
+            variant="ui-tiny"
+            color="neutral"
+            style={{ textAlign: 'right', marginTop: 4 }}
+          >
+            {this.state.characterCount} / {maxLength}
+          </Text>
+        )}
+        {error && errorMessage !== '' && (
+          <Text
+            variant="ui-tiny"
+            color="alert"
+            tag="div"
+            style={{ paddingLeft: 18, paddingTop: 8 }}
+          >
+            {errorMessage}
+          </Text>
+        )}
+      </>
+    );
+  }
+}
+
+Textarea.propTypes = {
+  /**
+   * Class name of the outer wrapper.
+   */
+  className: PropTypes.string,
+  /**
+   * Input label, if not empty this props will change default placeholder
+   */
+  label: PropTypes.string,
+  /**
+   * Set input default value
+   */
+  value: PropTypes.string,
+  /**
+   * Specify textarea row height
+   */
+  rows: PropTypes.number,
+  /**
+   * Set input to disable state
+   */
+  isDisabled: PropTypes.bool,
+  /**
+   * Set input to disable state
+   */
+  error: PropTypes.bool,
+  /**
+   * Error message text
+   */
+  errorMessage: PropTypes.string,
+  /**
+   * Set maximum character length if any
+   */
+  maxLength: PropTypes.number,
+  showCharacterCount: PropTypes.bool
+};
+
+Textarea.defaultProps = {
+  className: undefined,
+  rows: 2,
+  label: undefined,
+  value: '',
+  isDisabled: false,
+  error: false,
+  errorMessage: 'Wajib diisi',
+  showCharacterCount: false
+};
+
+export default Textarea;
---
+++ src/base/textarea/textarea.styles.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+import * as v from '../../shared/constants/variables';
+import * as c from '../../shared/constants/color';
+import * as ease from '../../shared/constants/easing';
+
+export const Container = styled.div`
+  border-radius: ${v.inputBorderRadiusLg}px;
+  border: 1px solid ${v.inputBorderColor};
+  transition: border 0.3s ${ease.easeInQuad};
+  display: table;
+  border-spacing: 0;
+  border-collapse: separate;
+  width: 100%;
+  position: relative;
+
+  &[data-error] {
+    border-color: ${v.stateRedText};
+
+    label {
+      color: ${v.stateRedText};
+    }
+  }
+`;
+
+export const Field = styled.textarea`
+  width: 100%;
+  font-size: ${v.FontSize.small}px;
+  background: ${v.inputBg};
+  padding: ${v.inputPaddingY}px ${v.inputPaddingX}px;
+  border-radius: ${v.inputBorderRadiusLg}px;
+  width: 100%;
+  border: 0;
+  font-family: ${v.fontFamilyBase};
+  position: relative;
+  color: ${c.DarkPrimary};
+  outline: 0;
+  resize: vertical;
+
+  &:disabled {
+    background: ${c.LightSecondary};
+    opacity: 0.5;
+  }
+`;
+
+export const Label = styled.label`
+  color: #757575;
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: ${v.spacing.m}px;
+  top: ${v.spacing.s + 2}px;
+  transition: 300ms ease all;
+  font-size: ${v.FontSize.small}px;
+
+  &[data-active='true'] {
+    top: -10px;
+    font-size: 14px;
+    color: #b0b2bb;
+    background: white;
+    padding: 0 ${v.spacing.xs}px;
+    left: ${v.spacing.xs}px;
+  }
+`;
+
+export const ErrorLabel = styled.span`
+  color: ${v.stateRedText};
+  font-family: ${v.fontFamilyBase};
+  display: block;
+  font-size: ${v.FontSize.tiny};
+`;
